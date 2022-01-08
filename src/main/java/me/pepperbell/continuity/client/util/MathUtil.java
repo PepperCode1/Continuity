310add50eacce3fa4833ab87c1488dbc1eb1eb86
PepperCode1 <44146161+PepperCode1@users.noreply.github.com>
2022-01-07 17:06:19 -0800

    Rewrite ctm_compact logic
    - Split correctly if UVs do not contain the entire sprite
    - Split so that the fewest number of quads is produced
    - Update build dependencies and Gradle
    - Bump version

--- src/main/java/me/pepperbell/continuity/client/util/MathUtil.java
+++ src/main/java/me/pepperbell/continuity/client/util/MathUtil.java
@@ -11,23 +11,19 @@ public final class MathUtil {
 		return 0;
 	}
 
-	public static float average(float a, float b) {
-		return (a + b) * 0.5f;
+	public static int lerp(float delta, int start, int end) {
+		return start + (int) (delta * (end - start));
 	}
 
-	public static int average(int a, int b) {
-		return (a + b) / 2;
+	public static int lerpColor(float delta, int colorA, int colorB) {
+		return (lerp(delta, colorA >> 24 & 0xFF, colorB >> 24 & 0xFF) << 24)
+				| (lerp(delta, colorA >> 16 & 0xFF, colorB >> 16 & 0xFF) << 16)
+				| (lerp(delta, colorA >> 8 & 0xFF, colorB >> 8 & 0xFF) << 8)
+				| (lerp(delta, colorA & 0xFF, colorB & 0xFF));
 	}
 
-	public static int averageColor(int color1, int color2) {
-		return (average((color1 >>> 24) & 0xFF, (color2 >>> 24) & 0xFF) << 24)
-				| (average((color1 >>> 16) & 0xFF, (color2 >>> 16) & 0xFF) << 16)
-				| (average((color1 >>> 8) & 0xFF, (color2 >>> 8) & 0xFF) << 8)
-				| (average(color1 & 0xFF, color2 & 0xFF));
-	}
-
-	public static int averageLight(int light1, int light2) {
-		return (average((light1 >> 20) & 0xFF, (light2 >> 20) & 0xFF) << 20)
-				| (average((light1 >> 4) & 0xFF, (light2 >> 4) & 0xFF) << 4);
+	public static int lerpLight(float delta, int lightA, int lightB) {
+		return (lerp(delta, lightA >> 20 & 0xF, lightB >> 20 & 0xF) << 20)
+				| (lerp(delta, lightA >> 4 & 0xF, lightB >> 4 & 0xF) << 4);
 	}
 }
