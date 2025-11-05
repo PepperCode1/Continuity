9baf8725ae80ebeda4ecf416cc9c07fa1200f1cc
刘冬煜 <liudongyu@lattebank.com>
2025-11-05 09:29:26 +0800

    Revert "失败提示信息"
    This reverts commit 61c7ef2f8eba43d9d5c55e5cafed035f191d8b54.

--- src/main/java/me/pepperbell/continuity/client/mixin/AtlasLoaderMixin.java
+++ src/main/java/me/pepperbell/continuity/client/mixin/AtlasLoaderMixin.java
@@ -69,11 +69,7 @@ abstract class AtlasLoaderMixin {
 							Optional<Resource> optionalResource = resourceManager.getResource(emissiveLocation);
 							if (optionalResource.isPresent()) {
 								Resource resource = optionalResource.get();
-								emissiveSuppliers.put(emissiveId, () -> try {
-									return SpriteLoader.load(emissiveId, resource);
-								} catch (Exception e) {
-									throw new RuntimeException("Failed to load emissive texture for " + emissiveId, e);
-								});
+								emissiveSuppliers.put(emissiveId, () -> SpriteLoader.load(emissiveId, resource));
 								emissiveIdMap.put(id, emissiveId);
 							}
 						} else {
